8bc11e7b3cb68e41c950b64fe409ecd5fde6246f
Marcos <marcosmartinezpalermo@gmail.com>
2022-04-04 13:33:47 -0300

    Fixed navbar icon overflowing

--- apps/container/src/app/navbar/navbar.tsx
+++ apps/container/src/app/navbar/navbar.tsx
@@ -5,8 +5,8 @@ import { Tonic3Logo } from '@gms-micro/assets';
 export function Navbar() {
     return (
         <Box w={'full'} h={'8vh'} bgColor={'black'} mb={0}>
-            <Flex w={'full'} h={'full'} py={5} justifyContent={'center'} alignItems={'center'}>
-                <Image src={Tonic3Logo} alt={"Tonic3"} w={'5rem'} />
+            <Flex w={'full'} h={'full'} py={2} justifyContent={'center'} alignItems={'center'}>
+                <Image src={Tonic3Logo} alt={"Tonic3"} h={'full'} />
             </Flex>
         </Box>
     );

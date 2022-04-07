77821803c07a32c211ad8fedb0b6430f2036b14c
Marcos <marcosmartinezpalermo@gmail.com>
2022-04-07 11:55:17 -0300

    Server deploy files

---
+++ dockerfile
@@ -0,0 +1,23 @@
+FROM node:17.4
+ARG PORT=80
+ARG PUBLIC_URL
+
+ENV PORT=$PORT
+ENV PUBLIC_URL=$PUBLIC_URL
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# add `/app/node_modules/.bin` to $PATH
+ENV PATH /app/node_modules/.bin:$PATH
+
+# install app dependencies
+COPY build ./build
+COPY package-server.json ./package.json
+RUN npm install --silent
+
+# add app
+COPY server.js ./
+
+EXPOSE $PORT
+CMD [ "node", "server.js" ]
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const mainServer = express();
+const loginServer = express();
+const reportsServer = express();
+const servers = [mainServer, loginServer, reportsServer];
+
+servers.forEach(server => {
+    server.use(helmet({ contentSecurityPolicy: false }));
+    // @ts-ignore
+    server.use(express.urlencoded({ extended: true, strict: false }));
+    server.use(express.json());
+    server.use(cors());
+})
+
+const basePath = process.env.PUBLIC_URL ? process.env.PUBLIC_URL : '';
+// eslint-disable-next-line no-undef
+mainServer.use(`${basePath}/`, express.static(`${__dirname}/dist/apps/container`));
+// eslint-disable-next-line no-undef
+loginServer.use(`${basePath}/`, express.static(`${__dirname}/dist/apps/login`));
+// eslint-disable-next-line no-undef
+reportsServer.use(`${basePath}/`, express.static(`${__dirname}/dist/apps/reports`));
+
+mainServer.get(`${basePath}*`, (req, res) => {
+    res.sendFile(`${__dirname}/dist/apps/container/index.html`);
+});
+
+loginServer.get(`${basePath}*`, (req, res) => {
+    res.sendFile(`${__dirname}/dist/apps/login/index.html`);
+});
+
+reportsServer.get(`${basePath}*`, (req, res) => {
+    res.sendFile(`${__dirname}/dist/apps/reports/index.html`);
+});
+
+// eslint-disable-next-line no-undef
+const mainPort = process.env.PORT ? process.env.PORT : 4200;
+const loginPort = 3001;
+const reportsPort = 3002;
+
+mainServer.listen(mainPort, () => console.log(`Example app listening on port ${mainPort}`));
+loginServer.listen(loginPort, () => console.log(`Example app listening on port ${loginPort}`));
+reportsServer.listen(reportsPort, () => console.log(`Example app listening on port ${reportsPort}`));

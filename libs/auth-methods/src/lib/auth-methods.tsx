19ccfcb3e5d7544c5f44ab39f6b44645ca983a2b
Marcos <marcosmartinezpalermo@gmail.com>
2022-05-13 12:39:43 -0300

    Log out button

--- libs/auth-methods/src/lib/auth-methods.tsx
+++ libs/auth-methods/src/lib/auth-methods.tsx
@@ -22,8 +22,8 @@ const getAuthInfo: (source: string) => LocalAuthInfo | null = (source: string) =
     // Delete the parameters from the URL but without reloading the page
     window.history.replaceState({}, document.title, `/${source}`);
 
-    return { 
-        authHeader: queryAuthHeader.toString(), 
+    return {
+        authHeader: queryAuthHeader.toString(),
         authUser: JSON.parse(queryAuthUser.toString()) as ApplicationUserPrivate
     }
 }
@@ -36,4 +36,23 @@ export const getAuthHeader: (source: string) => string | null = (source: string)
 export const getAuthUser: (source: string) => ApplicationUserPrivate | null = (source: string) => {
     const authInfo = getAuthInfo(source);
     return authInfo ? authInfo.authUser : null;
+}
+
+// Clears all cookies and reload the page
+export const signOut = () => {
+    var cookies = document.cookie.split("; ");
+    for (var c = 0; c < cookies.length; c++) {
+        var d = window.location.hostname.split(".");
+        while (d.length > 0) {
+            var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+            var p = location.pathname.split('/');
+            document.cookie = cookieBase + '/';
+            while (p.length > 0) {
+                document.cookie = cookieBase + p.join('/');
+                p.pop();
+            };
+            d.shift();
+        }
+    }
+    window.location.reload();
 }

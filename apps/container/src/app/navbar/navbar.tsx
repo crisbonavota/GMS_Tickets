4855308f583a95b5b2b71b12ca0374b1501c57e5
Marcos <marcosmartinezpalermo@gmail.com>
2022-05-13 12:26:26 -0300

    Current app on navbar

--- apps/container/src/app/navbar/navbar.tsx
+++ apps/container/src/app/navbar/navbar.tsx
@@ -1,13 +1,21 @@
-import { Box, Flex, Image } from '@chakra-ui/react';
+import { Box, Image, Link, Text, HStack, Heading, VStack } from '@chakra-ui/react';
 import { Tonic3Logo } from '@gms-micro/assets';
+import { config } from '@gms-micro/deploy';
 
 
 export function Navbar() {
+    const currentApp = config.apps.find(app => app.path === window.location.pathname.substring(1));
     return (
         <Box w={'full'} h={'8vh'} bgColor={'black'} mb={0}>
-            <Flex w={'full'} h={'full'} py={2} justifyContent={'center'} alignItems={'center'}>
-                <Image src={Tonic3Logo} alt={"Tonic3"} h={'full'} />
-            </Flex>
+            <HStack w={'full'} h={'full'} py={2} ps={10} justifyContent={'flex-start'} alignItems={'center'} spacing={5}>
+                <Link href={'/'} h={'full'}>
+                    <Image src={Tonic3Logo} alt={"Tonic3"} h={'full'} />
+                </Link>
+                <VStack alignItems={'flex-start'} spacing={0}>
+                    <Text color={'white'} fontSize={'sm'}>{currentApp?.module}</Text>
+                    <Heading fontSize={'2xl'} color={'white'}>{currentApp?.label}</Heading>
+                </VStack>
+            </HStack>
         </Box>
     );
 }

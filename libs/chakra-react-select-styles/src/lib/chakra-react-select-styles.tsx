b4556f5e3406a906dd08a85c8a216cad076d9ac0
Marcos <marcosmartinezpalermo@gmail.com>
2022-05-19 15:52:22 -0300

    Fixed dropdown options getting truncated

--- libs/chakra-react-select-styles/src/lib/chakra-react-select-styles.tsx
+++ libs/chakra-react-select-styles/src/lib/chakra-react-select-styles.tsx
@@ -1,14 +1,20 @@
-export const chakraSelectStyle = {
+import { ChakraStylesConfig } from 'chakra-react-select';
+
+export const chakraSelectStyle: ChakraStylesConfig = {
     control: (provided: any, state: any) => ({
         ...provided,
         borderColor: 'lightgray',
         borderWidth: 1,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
     }),
     dropdownIndicator: (provided: any) => ({
         ...provided,
         bgColor: 'white',
         color: '#3B8A7F',
-        borderWidth: 0
-    })
+        borderWidth: 0,
+    }),
+    menuList: (provided: any) => ({
+        ...provided,
+        minWidth: 'fit-content',
+    }),
 };
--- libs/query-utils/src/lib/query-utils.tsx
+++ libs/query-utils/src/lib/query-utils.tsx
@@ -7,57 +7,78 @@ import { useState, useMemo, useEffect } from 'react';
 import { useAuthHeader } from 'react-auth-kit';
 
 type QuerySelectProps = {
-    resource: string,
-    labelOption?: string,
-    valueOption?: string,
-    title: string,
-    value?: any,
-    setValue: (value?: any) => void
-}
+    resource: string;
+    labelOption?: string;
+    valueOption?: string;
+    title: string;
+    value?: any;
+    setValue: (value?: any) => void;
+};
 
 /// This function returns a dropdown based on ChakraUI + React-Select, the values of the dropdown come from the resource endpoint
-export const QuerySelect = ({resource, labelOption = "name", valueOption = "id", title, value, setValue }: QuerySelectProps) => {
+export const QuerySelect = ({
+    resource,
+    labelOption = 'name',
+    valueOption = 'id',
+    title,
+    value,
+    setValue,
+}: QuerySelectProps) => {
     const getAuthHeader = useAuthHeader();
-    const query = useQuery([resource], () => getResourceList(resource, getAuthHeader()));
-    const [internalValue, setInternalValue] = useState<{ label: string, value: any } | null>(null);
+    const query = useQuery([resource], () =>
+        getResourceList(resource, getAuthHeader())
+    );
+    const [internalValue, setInternalValue] = useState<{
+        label: string;
+        value: any;
+    } | null>(null);
 
-    const getOptions = useMemo(() => (data: any[]) => {
-        return data.map(item => {
-            return {
-                value: (item as any)[valueOption],
-                label: (item as any)[labelOption]
-            }
-        });
-    }, []);
+    const getOptions = useMemo(
+        () => (data: any[]) => {
+            return data.map((item) => {
+                return {
+                    value: (item as any)[valueOption],
+                    label: (item as any)[labelOption],
+                };
+            });
+        },
+        []
+    );
 
-    const onChange = useMemo(() => (element: any) => {
-        setInternalValue(element);
-        setValue(element.value);
-    }, []);
+    const onChange = useMemo(
+        () => (element: any) => {
+            setInternalValue(element);
+            setValue(element.value);
+        },
+        []
+    );
 
     // This is for when an external component changes the value (clicking on edit an item for example)
     useEffect(() => {
         if (!value) return setInternalValue(null);
         if (query.isSuccess) {
-            const element = getOptions(query.data.data).find(item => item.value === value);
+            const element = getOptions(query.data.data).find(
+                (item) => item.value === value
+            );
             element && setInternalValue(element);
         }
-    }, [value])
+    }, [value]);
 
     return (
         <VStack alignItems={'flex-start'}>
             <Heading fontSize={'md'}>{title}</Heading>
             {query.isLoading && <Skeleton w={'full'} h={'2.4rem'} />}
             <Box w={'full'}>
-                {query.isSuccess &&
+                {query.isSuccess && (
                     <Select
-                        size='md'
+                        size="md"
                         options={getOptions(query.data.data)}
                         chakraStyles={chakraSelectStyle}
                         value={internalValue}
                         onChange={onChange}
-                    />}
+                    />
+                )}
             </Box>
         </VStack>
-    )
-}
+    );
+};

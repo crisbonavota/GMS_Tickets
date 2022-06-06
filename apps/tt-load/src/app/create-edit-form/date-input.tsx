1798d6ff48264b251d571a8c449c652c7bc7b0a9
Gianmarco <mantovanigianm@gmail.com>
2022-06-06 16:49:56 -0300

    Added arrows for date filter at timetracker

--- apps/tt-load/src/app/create-edit-form/date-input.tsx
+++ apps/tt-load/src/app/create-edit-form/date-input.tsx
@@ -1,14 +1,42 @@
-import { VStack, Input, Heading } from '@chakra-ui/react';
+import { VStack, Input, Heading, IconButton, HStack } from '@chakra-ui/react';
+import moment from 'moment';
+import { useCallback } from 'react';
+import { GrNext, GrPrevious } from 'react-icons/gr';
 
 type Props = {
     date: string;
     setDate: (date: string) => void;
 };
-
 const DateInput = ({ date, setDate }: Props) => {
+    const onArrowClick = useCallback((add: boolean) => {
+        const currentDate = moment(date, 'YYYY-MM-DD');
+        const newDate = currentDate.add(add? 1 : -1, 'days').format('YYYY-MM-DD');
+        setDate(newDate);	
+    }, [date, setDate]);
+
     return (
         <VStack alignItems={'flex-start'}>
-            <Heading fontSize={'md'}>Date</Heading>
+            <HStack w='full' justifyContent={'space-between'}>
+                <Heading fontSize={'md'}>Date</Heading>
+                <HStack>
+                    <IconButton
+                        size={'sm'}
+                        icon={<GrPrevious/>} 
+                        cursor={'pointer'} 
+                        onClick={() => onArrowClick(false)} 
+                        aria-label="Previous day"
+                        isDisabled={date == ""}
+                    />
+                    <IconButton 
+                        size={'sm'} 
+                        icon={<GrNext/>} 
+                        cursor={'pointer'} 
+                        onClick={() => onArrowClick(true)}
+                        aria-label="Next day"
+                        isDisabled={date == ""}
+                    />
+                </HStack>  
+            </HStack>
             <Input
                 type={'date'}
                 bgColor={'white'}

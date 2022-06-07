d1887147b7600fe0e5aee8a46031de2a2307acd6
Marcos <marcosmartinezpalermo@gmail.com>
2022-06-07 16:53:31 -0300

    Integrated moment on dates filter on tt reports

--- apps/tt-reports/src/app/app.tsx
+++ apps/tt-reports/src/app/app.tsx
@@ -1,39 +1,60 @@
-import { Heading, VStack, Text, Input } from '@chakra-ui/react';
+import { VStack, Text, Input } from '@chakra-ui/react';
 import { useEffect, useState, useMemo } from 'react';
-import { useQuery } from 'react-query'
+import { useQuery } from 'react-query';
 import TableComponent from './table/table';
-import { TableDatesFilterWithChakra, TablePaginationWithChakra } from '@gms-micro/table-utils';
-import { getResourceListFilteredAndPaginated, TimetrackItem } from '@gms-micro/api-utils';
+import { TablePaginationWithChakra } from '@gms-micro/table-utils';
+import {
+    getResourceListFilteredAndPaginated,
+    TimetrackItem,
+} from '@gms-micro/api-utils';
 import SelectFilters from './select-filters/select-filters';
 import { useDidMountEffect } from '@gms-micro/react-hooks';
 import ExportModule from './export-module/export-module';
 import { Sort } from '@gms-micro/api-filters';
 import moment from 'moment';
 import { useAuthHeader } from 'react-auth-kit';
+import DatesFilter from './dates-filters/dates-filters';
 
 const App = () => {
     const [currentPage, setCurrentPage] = useState(0);
-    const [from, setFrom] = useState<string>(moment().add(-1, "days").format("YYYY-MM-DD")); // Default filter to 1 month ago
-    const [to, setTo] = useState<string>("");
+    const [from, setFrom] = useState<string>(
+        moment().add(-1, 'month').startOf('month').format('YYYY-MM-DD')
+    ); // Default filter to beggining of previous month
+    const [to, setTo] = useState<string>('');
     const [users, setUsers] = useState<Array<number>>([]);
-    const [sort, setSort] = useState<Sort>({ field: "date", isAscending: false });
+    const [sort, setSort] = useState<Sort>({
+        field: 'date',
+        isAscending: false,
+    });
     const [businessUnits, setBusinessUnits] = useState<Array<number>>([]);
     const [projects, setProjects] = useState<Array<number>>([]);
     const [proposals, setProposals] = useState<Array<number>>([]);
     const [accounts, setAccounts] = useState<Array<number>>([]);
-    const [generalSearch, setGeneralSearch] = useState("");
+    const [generalSearch, setGeneralSearch] = useState('');
     const [refetchAux, setRefetchAux] = useState(0);
     const getAuthHeader = useAuthHeader();
 
-    const onGeneralSearchChange = useMemo(() =>
-        (e: React.ChangeEvent<HTMLInputElement>) => {
+    const onGeneralSearchChange = useMemo(
+        () => (e: React.ChangeEvent<HTMLInputElement>) => {
             setGeneralSearch(e.target.value);
-        }, []);
+        },
+        []
+    );
 
     // Go to first page if any filter changes
     useEffect(() => {
         setCurrentPage(0);
-    }, [from, to, refetchAux, users, businessUnits, projects, proposals, accounts, sort]);
+    }, [
+        from,
+        to,
+        refetchAux,
+        users,
+        businessUnits,
+        projects,
+        proposals,
+        accounts,
+        sort,
+    ]);
 
     // Workaround for waiting 1500ms after user finished typing in the general searchbar for the search to be triggered
     useDidMountEffect(() => {
@@ -43,39 +64,98 @@ const App = () => {
         return () => clearTimeout(timeOutId);
     }, [generalSearch]);
 
-    const filters = useMemo(() => [
-        { field: 'date_bgr', value: from !== "" ? from : undefined },
-        { field: 'date_sml', value: to !== "" ? to : undefined },
-        { field: 'legacyUserId_OR', value: users.length ? users.join(',') : undefined },
-        { field: 'legacyUser.businessUnitId_OR', value: businessUnits.length ? businessUnits.join(',') : undefined },
-        { field: 'projectId_OR', value: projects.length ? projects.join(',') : undefined },
-        { field: 'project.proposalId_OR', value: proposals.length ? proposals.join(',') : undefined },
-        { field: 'project.proposal.accountId_OR', value: accounts.length ? accounts.join(',') : undefined },
-    ], [from, to, refetchAux, users, businessUnits, projects, proposals, accounts]);
+    const filters = useMemo(
+        () => [
+            { field: 'date_bgr', value: from !== '' ? from : undefined },
+            { field: 'date_sml', value: to !== '' ? to : undefined },
+            {
+                field: 'legacyUserId_OR',
+                value: users.length ? users.join(',') : undefined,
+            },
+            {
+                field: 'legacyUser.businessUnitId_OR',
+                value: businessUnits.length
+                    ? businessUnits.join(',')
+                    : undefined,
+            },
+            {
+                field: 'projectId_OR',
+                value: projects.length ? projects.join(',') : undefined,
+            },
+            {
+                field: 'project.proposalId_OR',
+                value: proposals.length ? proposals.join(',') : undefined,
+            },
+            {
+                field: 'project.proposal.accountId_OR',
+                value: accounts.length ? accounts.join(',') : undefined,
+            },
+        ],
+        [
+            from,
+            to,
+            refetchAux,
+            users,
+            businessUnits,
+            projects,
+            proposals,
+            accounts,
+        ]
+    );
 
-    const customFilters = useMemo(() => [{ name: 'generalSearch', value: generalSearch }], [generalSearch]);
+    const customFilters = useMemo(
+        () => [{ name: 'generalSearch', value: generalSearch }],
+        [generalSearch]
+    );
 
-    const refetchTriggers = useMemo(() =>
-        [currentPage, from, to, refetchAux, users, businessUnits, projects, proposals, accounts, sort],
-        [currentPage, from, to, refetchAux, users, businessUnits, projects, proposals, accounts, sort]);
+    const refetchTriggers = useMemo(
+        () => [
+            currentPage,
+            from,
+            to,
+            refetchAux,
+            users,
+            businessUnits,
+            projects,
+            proposals,
+            accounts,
+            sort,
+        ],
+        [
+            currentPage,
+            from,
+            to,
+            refetchAux,
+            users,
+            businessUnits,
+            projects,
+            proposals,
+            accounts,
+            sort,
+        ]
+    );
 
-    const timetrackQuery = useQuery(
-        ['timetrack', refetchTriggers],
-        () => getResourceListFilteredAndPaginated<TimetrackItem>(
-            "timetrack",
+    const timetrackQuery = useQuery(['timetrack', refetchTriggers], () =>
+        getResourceListFilteredAndPaginated<TimetrackItem>(
+            'timetrack',
             getAuthHeader(),
             filters,
             customFilters,
             sort,
-            currentPage)
+            currentPage
+        )
     );
 
     return (
         <VStack w={'full'} maxW={'full'} p={5}>
             <VStack w={'90%'} spacing={5}>
-                <Heading fontSize={'2xl'}>Timetrack reports</Heading>
                 <VStack w={'full'} spacing={5} alignItems={'flex-start'}>
-                    <TableDatesFilterWithChakra isLoading={false} from={from} to={to} setFrom={setFrom} setTo={setTo} full />
+                    <DatesFilter
+                        from={from}
+                        to={to}
+                        setFrom={setFrom}
+                        setTo={setTo}
+                    />
                     <Input
                         w={'full'}
                         bgColor={'white'}
@@ -85,27 +165,78 @@ const App = () => {
                     />
                     <SelectFilters
                         dropdownsData={[
-                            { labelOption: 'fullName', valueOption: 'id', values: users, setValue: setUsers, title: "Employee", resource: 'users/legacy' },
-                            { labelOption: 'name', valueOption: 'id', values: businessUnits, setValue: setBusinessUnits, title: "Business Unit", resource: 'businessUnits' },
-                            { labelOption: 'name', valueOption: 'id', values: projects, setValue: setProjects, title: "Project", resource: 'projects' },
-                            { labelOption: 'name', valueOption: 'id', values: proposals, setValue: setProposals, title: "Proposal", resource: 'proposals' },
-                            { labelOption: 'name', valueOption: 'id', values: accounts, setValue: setAccounts, title: "Account", resource: 'accounts' }
+                            {
+                                labelOption: 'fullName',
+                                valueOption: 'id',
+                                values: users,
+                                setValue: setUsers,
+                                title: 'Employee',
+                                resource: 'users/legacy',
+                            },
+                            {
+                                labelOption: 'name',
+                                valueOption: 'id',
+                                values: businessUnits,
+                                setValue: setBusinessUnits,
+                                title: 'Business Unit',
+                                resource: 'businessUnits',
+                            },
+                            {
+                                labelOption: 'name',
+                                valueOption: 'id',
+                                values: projects,
+                                setValue: setProjects,
+                                title: 'Project',
+                                resource: 'projects',
+                            },
+                            {
+                                labelOption: 'name',
+                                valueOption: 'id',
+                                values: proposals,
+                                setValue: setProposals,
+                                title: 'Proposal',
+                                resource: 'proposals',
+                            },
+                            {
+                                labelOption: 'name',
+                                valueOption: 'id',
+                                values: accounts,
+                                setValue: setAccounts,
+                                title: 'Account',
+                                resource: 'accounts',
+                            },
                         ]}
                     />
-                    <ExportModule filters={filters} customFilters={customFilters} refetch={refetchTriggers} />
+                    <ExportModule
+                        filters={filters}
+                        customFilters={customFilters}
+                        refetch={refetchTriggers}
+                    />
                 </VStack>
                 {timetrackQuery.isLoading && <Text>Loading...</Text>}
-                {timetrackQuery.isSuccess && <TableComponent tableData={timetrackQuery.data.data} sort={sort} setSort={setSort} />}
-                {timetrackQuery.isError && <Text>There was an error generating the table, try again later</Text>}
+                {timetrackQuery.isSuccess && (
+                    <TableComponent
+                        tableData={timetrackQuery.data.data}
+                        sort={sort}
+                        setSort={setSort}
+                    />
+                )}
+                {timetrackQuery.isError && (
+                    <Text>
+                        There was an error generating the table, try again later
+                    </Text>
+                )}
                 <TablePaginationWithChakra
                     isLoading={timetrackQuery.isLoading}
                     currentPage={currentPage}
                     setCurrentPage={setCurrentPage}
-                    pagesAmountHeader={timetrackQuery.data?.headers['pages-amount']}
+                    pagesAmountHeader={
+                        timetrackQuery.data?.headers['pages-amount']
+                    }
                 />
             </VStack>
         </VStack>
-    )
-}
+    );
+};
 
-export default App
+export default App;
--- libs/table-utils/src/lib/table-utils.tsx
+++ libs/table-utils/src/lib/table-utils.tsx
@@ -1,18 +1,7 @@
-import {
-    chakra,
-    HStack,
-    IconButton,
-    Select,
-    Text,
-    VStack,
-} from '@chakra-ui/react';
+import { HStack, IconButton, Text, VStack } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
 import { MdSkipNext, MdSkipPrevious } from 'react-icons/md';
-import { useQuery } from 'react-query';
-import { getResourceList } from '@gms-micro/api-utils';
-import { LegacyUserPublic } from '@gms-micro/auth-types';
-import { useAuthHeader } from 'react-auth-kit';
 
 export interface TablePaginationWithChakraProps {
     currentPage: number;
@@ -83,115 +72,6 @@ export const TablePaginationWithChakra = ({
                     </Text>{' '}
                     elements
                 </Text>
-            )}
-        </VStack>
-    );
-};
-
-export interface TableDatesFilterWithChakraProps {
-    from: string;
-    to: string;
-    setFrom: (date: string) => void;
-    setTo: (date: string) => void;
-    isLoading: boolean;
-    full?: boolean;
-}
-
-export const TableDatesFilterWithChakra = ({
-    from,
-    to,
-    setFrom,
-    setTo,
-    isLoading,
-    full,
-}: TableDatesFilterWithChakraProps) => {
-    const onFromChange = useMemo(
-        () => (e: React.ChangeEvent<HTMLInputElement>) => {
-            setFrom(e.target.value);
-            to && Date.parse(to) < Date.parse(e.target.value) && setTo('');
-        },
-        []
-    );
-
-    const onToChange = useMemo(
-        () => (e: React.ChangeEvent<HTMLInputElement>) => {
-            setTo(e.target.value);
-            from &&
-                Date.parse(from) > Date.parse(e.target.value) &&
-                setFrom('');
-        },
-        []
-    );
-
-    return (
-        <HStack w={full ? 'full' : undefined}>
-            <VStack alignItems={'flex-start'} flex={full ? 1 : undefined}>
-                <Text fontSize={'sm'}>From</Text>
-                <chakra.input
-                    disabled={isLoading}
-                    onChange={onFromChange}
-                    value={from}
-                    p={1.5}
-                    borderRadius={5}
-                    borderColor={'lightgray'}
-                    borderWidth={1}
-                    type={'date'}
-                    w={full ? 'full' : undefined}
-                />
-            </VStack>
-            <VStack alignItems={'flex-start'} flex={full ? 1 : undefined}>
-                <Text fontSize={'sm'}>To</Text>
-                <chakra.input
-                    disabled={isLoading}
-                    onChange={onToChange}
-                    value={to}
-                    p={1.5}
-                    borderRadius={5}
-                    borderColor={'lightgray'}
-                    borderWidth={1}
-                    type={'date'}
-                    w={full ? 'full' : undefined}
-                />
-            </VStack>
-        </HStack>
-    );
-};
-
-export interface TableLegacyUserFilterWithChakraProps {
-    legacyUser: string;
-    setLegacyUser: (legacyUser: string) => void;
-    isLoading: boolean;
-}
-
-export const TableSingleLegacyUserFilterWithChakra = ({
-    legacyUser,
-    setLegacyUser,
-    isLoading,
-}: TableLegacyUserFilterWithChakraProps) => {
-    const getAuthHeader = useAuthHeader();
-    const query = useQuery(['legacyUsers'], () =>
-        getResourceList<LegacyUserPublic>('users/legacy', getAuthHeader())
-    );
-    return (
-        <VStack alignItems={'flex-start'} w={'15rem'}>
-            <Text fontSize={'sm'}>Employee</Text>
-            {query.isSuccess && (
-                <Select
-                    w={'full'}
-                    bgColor={'white'}
-                    value={legacyUser}
-                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-                        setLegacyUser(e.target.value)
-                    }
-                    disabled={isLoading}
-                >
-                    <option value={''}>All</option>
-                    {query.data.data.map((user) => (
-                        <option key={user.id} value={user.id}>
-                            {user.fullName}
-                        </option>
-                    ))}
-                </Select>
             )}
         </VStack>
     );
